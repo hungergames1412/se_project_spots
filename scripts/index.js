438cb096369514af46075555a8a5ccbdcdbc3304
hungergames1412 <susan.hofmann@aol.com>
2025-09-24 06:28:56 -0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,10 @@
 const initialCards = [
-  {
-    name: "Val Thorens",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
-  },
-  {
-    name: "Restaurant terrace",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
-  },
-  {
-    name: "An outdoor cafe",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
-  },
-  {
-    name: "A very long bridge, over the forest and through the trees",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
-  },
-  {
-    name: "Tunnel with morning light",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
-  },
-  {
-    name: "Mountain house",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
-  },
+  { name: "Val Thorens", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg" },
+  { name: "Restaurant terrace", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg" },
+  { name: "An outdoor cafe", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg" },
+  { name: "A very long bridge, over the forest and through the trees", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg" },
+  { name: "Tunnel with morning light", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg" },
+  { name: "Mountain house", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg" },
 ];
 
 const editProfileBtn = document.querySelector(".profile__edit-btn");
@@ -39,6 +21,8 @@ const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 const addCardFormElement = newPostModal.querySelector(".modal__form");
 const descriptionInput = newPostModal.querySelector("#new-post-caption");
 const linkInput = newPostModal.querySelector("#new-post-link");
+const cardsList = document.querySelector(".cards__list"); // define this
+const cardTemplate = document.querySelector("#card-template").content; // grab template
 
 // --- Modal helpers ---
 function openModal(modal) {
@@ -51,7 +35,6 @@ function closeModal(modal) {
 
 // --- Edit profile modal ---
 editProfileBtn.addEventListener("click", function () {
-  // preload values into the form
   nameInput.value = profileNameElement.textContent;
   jobInput.value = profileJobElement.textContent;
   openModal(editProfileModal);
@@ -70,7 +53,6 @@ function handleProfileFormSubmit(evt) {
 
 profileFormElement.addEventListener("submit", handleProfileFormSubmit);
 
-
 newPostBtn.addEventListener("click", function () {
   openModal(newPostModal);
 });
@@ -82,8 +64,11 @@ newPostCloseBtn.addEventListener("click", function () {
 function handleAddCardSubmit(evt) {
   evt.preventDefault();
 
-  console.log("Description:", descriptionInput.value);
-  console.log("Link:", linkInput.value);
+  const newCard = getCardElement({
+    name: descriptionInput.value,
+    link: linkInput.value,
+  });
+  cardsList.prepend(newCard); // add new card to top
 
   closeModal(newPostModal);
   evt.target.reset();
@@ -91,11 +76,33 @@ function handleAddCardSubmit(evt) {
 
 addCardFormElement.addEventListener("submit", handleAddCardSubmit);
 
-//for (let i = 0; i < initialCards.length;i++) {
-//const cardElement = getCardElement(initialCards[i]);
-//cardsList.append(cardElement);
-//}
+// --- Create card function (missing before) ---
+function getCardElement(data) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
 
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const likeBtn = cardElement.querySelector(".card__like-btn");
+  const deleteBtn = cardElement.querySelector(".card__delete-btn");
+
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardTitle.textContent = data.name;
+
+  // Like button toggle
+  likeBtn.addEventListener("click", () => {
+    likeBtn.classList.toggle("card__like-btn_active");
+  });
+
+  // Delete card
+  deleteBtn.addEventListener("click", () => {
+    cardElement.remove();
+  });
+
+  return cardElement;
+}
+
+// --- Render initial cards ---
 initialCards.forEach((item) => {
   const cardElement = getCardElement(item);
   cardsList.append(cardElement);
