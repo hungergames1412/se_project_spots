edf378367a28d2dde7654551058277334de149a5
hungergames1412 <susan.hofmann@aol.com>
2025-09-25 07:44:00 -0500

    Update index.js - stage 8

--- scripts/index.js
+++ scripts/index.js
@@ -24,28 +24,93 @@ const initialCards = [
     name: "Mountain house",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
   },
+  {
+    name: "Landscape example",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg",
+  },
 ];
 
 // --- DOM Elements ---
 const cardsList = document.querySelector(".cards__list");
 const cardTemplate = document.querySelector("#card-template");
 
-// --- Function to create a card from data ---
+// --- Modals ---
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const newPostModal = document.querySelector("#new-post-modal");
+const imagePreviewModal = document.querySelector("#image-preview-modal");
+
+function openModal(modal) {
+  modal.classList.add("modal_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("modal_is-opened");
+}
+
+// --- Create Card ---
 function getCardElement(data) {
   const cardElement = cardTemplate.content.querySelector(".card").cloneNode(true);
 
   const cardImage = cardElement.querySelector(".card__image");
   const cardTitle = cardElement.querySelector(".card__title");
+  const likeButton = cardElement.querySelector(".card__like-button");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
 
   cardImage.src = data.link;
   cardImage.alt = data.name;
   cardTitle.textContent = data.name;
 
+  // --- Like button ---
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("card__like-button_active");
+  });
+
+  // --- Delete button ---
+  deleteButton.addEventListener("click", () => {
+    cardElement.remove();
+  });
+
+  // --- Image preview modal ---
+  cardImage.addEventListener("click", () => {
+    const modalImage = imagePreviewModal.querySelector(".modal__image");
+    const modalCaption = imagePreviewModal.querySelector(".modal__caption");
+
+    modalImage.src = data.link;
+    modalImage.alt = data.name;
+    modalCaption.textContent = data.name;
+
+    openModal(imagePreviewModal);
+  });
+
   return cardElement;
 }
 
-// --- Render Initial Cards ---
+// --- Render initial cards ---
 initialCards.forEach((item) => {
   const cardElement = getCardElement(item);
-  cardsList.append(cardElement); // add each card to the list
+  cardsList.append(cardElement);
 });
+
+// --- New Post ---
+const newPostForm = newPostModal.querySelector(".modal__form");
+const linkInput = newPostModal.querySelector("#new-post-link");
+const captionInput = newPostModal.querySelector("#new-post-caption");
+
+newPostForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+
+  const newCardData = {
+    name: captionInput.value,
+    link: linkInput.value,
+  };
+
+  const cardElement = getCardElement(newCardData);
+  cardsList.prepend(cardElement);
+
+  evt.target.reset();
+  closeModal(newPostModal);
+});
+
+// --- Close preview modal ---
+const imagePreviewCloseBtn = imagePreviewModal.querySelector(".modal__close-btn");
+imagePreviewCloseBtn.addEventListener("click", () => closeModal(imagePreviewModal));
