0930913f76d153021f67b88a9e25bedadaef58da
hungergames1412 <susan.hofmann@aol.com>
2025-09-25 07:38:27 -0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -27,98 +27,25 @@ const initialCards = [
 ];
 
 // --- DOM Elements ---
-const editProfileBtn = document.querySelector(".profile__edit-btn");
-const editProfileModal = document.querySelector("#edit-profile-modal");
-const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
-const profileNameElement = document.querySelector(".profile__name");
-const profileJobElement = document.querySelector(".profile__description");
-const profileFormElement = editProfileModal.querySelector(".modal__form");
-const nameInput = editProfileModal.querySelector("#profile-name-input");
-const jobInput = editProfileModal.querySelector("#profile-description-input");
+const cardsList = document.querySelector(".cards__list");
+const cardTemplate = document.querySelector("#card-template");
 
-const newPostBtn = document.querySelector(".profile__add-btn");
-const newPostModal = document.querySelector("#new-post-modal");
-const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
-const addCardFormElement = newPostModal.querySelector(".modal__form");
-const descriptionInput = newPostModal.querySelector("#new-post-caption");
-const linkInput = newPostModal.querySelector("#new-post-link");
-
-const cardsList = document.querySelector(".cards__list"); // <-- added missing selector
-
-// --- Modal Helpers ---
-function openModal(modal) {
-  modal.classList.add("modal_is-opened");
-}
-
-function closeModal(modal) {
-  modal.classList.remove("modal_is-opened");
-}
-
-// --- Edit Profile Modal ---
-editProfileBtn.addEventListener("click", function () {
-  nameInput.value = profileNameElement.textContent;
-  jobInput.value = profileJobElement.textContent;
-  openModal(editProfileModal);
-});
-
-editProfileCloseBtn.addEventListener("click", function () {
-  closeModal(editProfileModal);
-});
-
-function handleProfileFormSubmit(evt) {
-  evt.preventDefault();
-  profileNameElement.textContent = nameInput.value;
-  profileJobElement.textContent = jobInput.value;
-  closeModal(editProfileModal);
-}
-
-profileFormElement.addEventListener("submit", handleProfileFormSubmit);
-
-// --- New Post Modal ---
-newPostBtn.addEventListener("click", function () {
-  openModal(newPostModal);
-});
-
-newPostCloseBtn.addEventListener("click", function () {
-  closeModal(newPostModal);
-});
-
-function handleAddCardSubmit(evt) {
-  evt.preventDefault();
-
-  const newCard = {
-    name: descriptionInput.value,
-    link: linkInput.value,
-  };
-
-  const cardElement = getCardElement(newCard);
-  cardsList.prepend(cardElement); // add new card to top
-
-  closeModal(newPostModal);
-  evt.target.reset();
-}
-
-addCardFormElement.addEventListener("submit", handleAddCardSubmit);
-
-// --- Create Card Element ---
+// --- Function to create a card from data ---
 function getCardElement(data) {
-  const cardTemplate = document
-    .querySelector("#card-template")
-    .content.querySelector(".card")
-    .cloneNode(true);
+  const cardElement = cardTemplate.content.querySelector(".card").cloneNode(true);
 
-  const cardImage = cardTemplate.querySelector(".card__image");
-  const cardTitle = cardTemplate.querySelector(".card__title");
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
 
   cardImage.src = data.link;
   cardImage.alt = data.name;
   cardTitle.textContent = data.name;
 
-  return cardTemplate;
+  return cardElement;
 }
 
 // --- Render Initial Cards ---
 initialCards.forEach((item) => {
   const cardElement = getCardElement(item);
-  cardsList.append(cardElement);
+  cardsList.append(cardElement); // add each card to the list
 });
